fa3eda6565105c5251ea2dada4796bd81bf63642
Roshan Ekka <1ROSHANEKKA@GMAIL.COM>
2023-03-04 23:52:55 +0530

    added functionality

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        /* For image literals in Xcode 13 type:
+         
+         #imageLiteral(
+         
+         */
+        //who   what    value
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceThree")
+        
+        diceImageView1.alpha = 0.5
+        
+        
         // Do any additional setup after loading the view.
     }
-
 
 }
 
